55f1f8b8e225cf8fa6bcd0e4d7423ffdc31bbd76
Shaikh Mumsad Ahmed <113447217+Mumsad@users.noreply.github.com>
2023-07-14 16:13:17 +0000

    datatype

---
+++ pratice/01_variable.js
@@ -0,0 +1,20 @@
+const accountId=1444553;//Not changed 
+// You can use the variable name let and var 
+let accountEmail='mumsadking@gmail.com';
+var accountPassword="12345";
+accountcity="Mumbai";
+
+// accountId =3; // not allowed you cannot changed the value
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+accountEmail="kingkhan@gamil.com"
+accountPassword="786"
+accountcity="utterpradesh";
+
+
+console.log(accountEmail);
+
+console.table([accountEmail,accountPassword,accountcity]);
---
+++ pratice/datatype.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+ 
+// alert(3+5) we are using nodejs, not browser
+
+let name= 'musa'
+let age = 48
+let log=false
+
+// number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+//null => standalone value
+// undefined => ====value not assigned
+// symbol
+
+//object 
+console.log(typeof null) // object
+console.log(typeof undefined); // undefined 
+
+console.log("king ")
